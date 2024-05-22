3ac9a44bd3d70eff2871b1c9ed3d7509adf8c2bf
NivaJr <nivaldo.jr15@gmail.com>
2024-05-22 15:14:56 -0300

    .
    .

--- ipcfex3/app/index.js
+++ ipcfex3/app/index.js
@@ -1,15 +1,38 @@
-import { View } from 'react-native';
+import { Button, View } from 'react-native';
 import { Pressable, Text } from 'react-native';
 import { Link } from 'expo-router';
+import { useState } from 'react';
+import Saude from './pages/saude';
 
 
 export default function Page() {
+    const [const1, setConst1] = useState(true)
+    const [const2, setConst2] = useState(false)
   return (
     <View>
+        {
+            const1 &&
+            <Saude/>
+        }
+        {
+            const2 &&
+            <View>
+                <Text>teste 2</Text>
+            </View>
+        }
         <Link href="./pages/home" asChild>
-            <Pressable>
-                <Text>Nivaldo é bonito</Text>
-            </Pressable>
+            <View>
+                <Pressable >
+                    <Text>Nivaldo é bonito</Text>
+                </Pressable>
+                <Button title='Butão' onPress={()=>{
+                    setConst1(!const1)
+                    setConst2(!const2)
+                    console.log(const1, const2)
+                }}>
+                    
+                </Button>
+            </View>
         </Link>
     </View>
   );
